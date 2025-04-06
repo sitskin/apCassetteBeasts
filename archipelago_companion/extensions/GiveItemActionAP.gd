8be2c5a67a84e57b954d2f4b6d882e74ddf6704e
Sam Itskin <sitskin@gmail.com>
2025-04-06 16:49:25 -0500

    Mod: Add give item action removal of item when given to self.

--- archipelago_companion/extensions/GiveItemActionAP.gd
+++ archipelago_companion/extensions/GiveItemActionAP.gd
@@ -8,6 +8,8 @@ func _run():
 		item = ItemFactory.generate_item(apItem)
 		item.name = DLC.mods_by_id.archipelago_companion.archipelagoConnectionManager.getItemString(location)
 		item.description = "This will explain what AP item you got"
+	if item.name == "Self Item":
+		item = null
 	if item != null and item_amount > 0:
 		var co = MenuHelper.give_item(item, item_amount, passive_message)
 		if co is GDScriptFunctionState:
